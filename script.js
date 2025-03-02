d9320a86ba7c7b56cba265cd9059667948bb9f93
samirpuri204 <112630195+samirpuri204@users.noreply.github.com>
2025-03-02 20:00:00 +1100

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// Example JavaScript code
-document.addEventListener('DOMContentLoaded', function() {
-    console.log('Page fully loaded');
-    // Add any additional JavaScript functionality here if needed
-});
